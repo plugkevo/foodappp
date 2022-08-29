e002b448e40d6a7e01dc099d4ffd3a63fe8c6a69
plugkevo <munenekevin366@gmail.com>
2022-08-29 11:11:20 +0300

    back button

--- app/src/main/java/com/example/foodapp/IntroActivity.kt
+++ app/src/main/java/com/example/foodapp/IntroActivity.kt
@@ -1,15 +1,24 @@
 package com.example.foodapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import org.xml.sax.HandlerBase
 
 class IntroActivity : AppCompatActivity() {
-    private Button button;
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        },2000)
     }
+
+
+
 
 }
--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.foodapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2)
+
+        val buttonClick = findViewById<Button>(R.id.button_click)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
